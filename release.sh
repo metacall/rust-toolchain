ae65ed985e9d23e97c52fa1acd8d4eae1194f537
SATVIKsynopsis <futureiitianisme@gmail.com>
2026-06-02 02:41:22 +0530

    bookworm and trixie

--- release.sh
+++ release.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-docker buildx build --load -t metacall/rust-toolchain .
+docker buildx build \
+  --build-arg RUST_TOOLCHAIN_IMAGE="${RUST_TOOLCHAIN_IMAGE}" \
+  --load \
+  -t metacall/rust-toolchain .
 docker run --name metacall-rust-toolchain metacall/rust-toolchain
 docker cp metacall-rust-toolchain:/rust-toolchain.tar.gz ./dist/
 docker rm metacall-rust-toolchain
