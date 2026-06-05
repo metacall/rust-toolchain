6adda5fadc37602c775d6ba91619c8296ddcdb0f
SATVIKsynopsis <futureiitianisme@gmail.com>
2026-06-05 22:17:53 +0530

    multi-arch-build

--- release.sh
+++ release.sh
@@ -2,9 +2,11 @@
 set -euxo pipefail
 
 docker buildx build \
+  --platform "${PLATFORM:-linux/amd64}" \
   --build-arg RUST_TOOLCHAIN_IMAGE="${RUST_TOOLCHAIN_IMAGE}" \
   --load \
   -t metacall/rust-toolchain .
 docker run --name metacall-rust-toolchain metacall/rust-toolchain
-docker cp metacall-rust-toolchain:/rust-toolchain.tar.gz ./dist/
+docker cp metacall-rust-toolchain:/rust-toolchain-runtime.tar.gz ./dist/
+docker cp metacall-rust-toolchain:/rust-toolchain-dev.tar.gz ./dist/
 docker rm metacall-rust-toolchain
